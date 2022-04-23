ec591cc996dfa218d33189ae94116640b7ef5009
David Konsumer <konsumer@jetboystudio.com>
2022-04-22 18:32:40 -0700

    notes on math

--- async.js
+++ async.js
@@ -8,8 +8,8 @@ export async function MaintainFPS (targetFPS) {
   // only do it every-other frame
   const timetarget = 1 / targetFPS
   const timeframe = r.GetFrameTime()
-  const waittime = (timetarget - timeframe) * 2000
-  const realFPS = (1 / timeframe) * 2
+  const waittime = (timetarget - timeframe) * 2000 // 1000 is for conversion to ms, * 2 is because it seems to draw 2 frames (1 slower, 1 very fast)
+  const realFPS = (1 / timeframe) * 2 // it seems to draw 2 frames (1 slower, 1 very fast)
   if (waittime > 0) {
     await wait(waittime)
   }
