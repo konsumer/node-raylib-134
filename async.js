0b2dfbe07f9f5302d4daaee31f42245d2741a709
David Konsumer <konsumer@jetboystudio.com>
2022-04-22 18:11:39 -0700

    better FPS detection and display

--- async.js
+++ async.js
@@ -3,14 +3,17 @@ import r from 'raylib'
 // async wait
 export const wait = time => new Promise((resolve, reject) => { setTimeout(resolve, time) })
 
-// try to hit target FPS async
+let i = 0
 export async function MaintainFPS (targetFPS) {
   const timetarget = 1 / targetFPS
   const timeframe = r.GetFrameTime()
-  const waittime = (timetarget - timeframe) * 1000 * 30
+  const waittime = (timetarget - timeframe) * 2000
+  const realFPS = (1 / timeframe) * 2
   if (waittime > 0) {
-    console.log(waittime)
     await wait(waittime)
   }
-  return !r.WindowShouldClose()
+
+  // this should show a TICK every second
+  console.log(i++ % targetFPS === 0 ? 'TICK' : '')
+  return r.WindowShouldClose() ? false : realFPS
 }
--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const screenHeight = 450
 r.InitWindow(screenWidth, screenHeight, 'raylib [async] example - async game-loop')
 r.SetTargetFPS(0)
 
-while(await MaintainFPS(60)) {
+let fps = 0
+while(fps = await MaintainFPS(60)) {
   r.BeginDrawing()
   r.ClearBackground(r.RAYWHITE)
   r.DrawText('Congrats! You created your first window!', 190, 200, 20, r.LIGHTGRAY)
+  
+  // this is close to correct FPS
+  r.DrawText(fps.toString(), screenWidth - 100, 10, 20, r.BLACK)
+  
+  // this is inaccurate
   r.DrawFPS(10, 10)
+
   r.EndDrawing()
 }
 
